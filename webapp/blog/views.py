98c52fe0ef45cd3737885aeb2cd102d8154b182b
Tim Heap <tim@ionata.com.au>
2012-05-31 01:51:23 +1000

    Make single post view

--- webapp/blog/views.py
+++ webapp/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from blog.models import Post
 
 def index(request):
@@ -9,7 +9,11 @@ def index(request):
     })
 
 def view(request, slug):
-    pass
+    post = get_object_or_404(Post.objects.published(), slug=slug, published=True)
+
+    return render(request, 'blog/view.html', {
+        'post': post
+    })
 
 def archive(request, year=None, month=None, day=None):
     pass
