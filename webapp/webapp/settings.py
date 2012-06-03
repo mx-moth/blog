cb2cb3a7ec48de11d542c1c2a5fd62f88d6fa90a
Tim Heap <tim@ionata.com.au>
2012-06-03 19:10:41 +1000

    Put all assets in the repository

--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -48,24 +48,16 @@ USE_L10N = True
 # If you set this to False, Django will not use timezone-aware datetimes.
 USE_TZ = True
 
-# Absolute filesystem path to the directory that will hold user-uploaded files.
-# Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = ''
+ASSET_URL = '/assets/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = ''
-
-# Absolute path to the directory static files should be collected to.
-# Don't put anything in this directory yourself; store your static files
-# in apps' "static/" subdirectories and in STATICFILES_DIRS.
-# Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+MEDIA_URL = ASSET_URL + 'media/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = 'http://maelstrom.github.com/tumblr-theme/'
+STATIC_URL = ASSET_URL + 'static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
