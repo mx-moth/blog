4af50375182007708811475988445097319cd54a
Tim Heap <tim@ionata.com.au>
2012-06-07 22:45:26 +1000

    Add blog feed

---
+++ webapp/blog/feeds.py
@@ -0,0 +1,19 @@
+from django.contrib.syndication.views import Feed
+from django.contrib.sites.models import Site
+from django.core.urlresolvers import reverse_lazy
+
+from blog.models import Post
+
+class LatestPostsFeed(Feed):
+    title = "Latest posts - %s" % Site.objects.get_current().name
+    link = reverse_lazy("blog.index")
+    description = "The latest blog posts"
+
+    def items(self):
+        return Post.objects.all()[:10]
+    
+    def item_title(self, item):
+        return item.title
+
+    def item_description(self, item):
+        return item.body_html
--- webapp/blog/urls.py
+++ webapp/blog/urls.py
@@ -1,11 +1,16 @@
 from django.conf.urls import patterns, include, url
 
+from blog.views import archive, view, index
+from blog.feeds import LatestPostsFeed
+
 urlpatterns = patterns('blog.views', 
-    url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/$', 'archive', name='blog.archive.day'),
-    url(r'^(?P<year>\d{4})/(?P<month>\d{2})/$', 'archive', name='blog.archive.month'),
-    url(r'^(?P<year>\d{4})/$', 'archive', name='blog.archive.year'),
+    url(r'^feed.rss$', LatestPostsFeed(), name='blog.feed'), 
 
-    url(r'^(?P<slug>[a-zA-Z0-9_.-]+)$', 'view', name='blog.post'),
+    url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/$', archive, name='blog.archive.day'),
+    url(r'^(?P<year>\d{4})/(?P<month>\d{2})/$', archive, name='blog.archive.month'),
+    url(r'^(?P<year>\d{4})/$', archive, name='blog.archive.year'),
 
-    url(r'^$', 'index', name='blog.index'), 
+    url(r'^(?P<slug>[a-zA-Z0-9_.-]+)$', view, name='blog.post'),
+
+    url(r'^$', index, name='blog.index'), 
 )
