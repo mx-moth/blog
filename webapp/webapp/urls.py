99d4f89c3c37f54fe4bc08f6fdef0a20148a3911
Tim Heap <tim@ionata.com.au>
2012-06-11 18:28:58 +1000

    Add favicon

--- webapp/webapp/urls.py
+++ webapp/webapp/urls.py
@@ -1,13 +1,17 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views.generic.simple import redirect_to
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
+    url(r'^favicon.ico', redirect_to, {'url': settings.STATIC_URL + 'favicon.ico'}),
+
     url(r'^$', 'blog.views.index', name='home'),
     url(r'^b/', include('blog.urls')),
 
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
+
 )
